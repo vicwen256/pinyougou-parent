bde2048975e3c7b1afc32737e4379cae56d424cc
NeoBitWen <vicwen256@gmail.com>
2019-10-08 01:05:54 +0800

    商品管理与商家授权登录

---
+++ pinyougou-web-shop/src/main/java/cn/itcast/core/service/UserDetailServiceImpl.java
@@ -0,0 +1,37 @@
+package cn.itcast.core.service;
+
+import cn.itcast.core.pojo.seller.Seller;
+import cn.itcast.core.service.seller.SellerService;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import javax.annotation.Resource;
+import java.util.HashSet;
+import java.util.Set;
+
+public class UserDetailServiceImpl implements UserDetailsService {
+
+    private SellerService sellerService;
+
+    public void setSellerService(SellerService sellerService) {
+        this.sellerService = sellerService;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Seller seller = sellerService.findOne(username);
+        if (seller != null && "1".equals(seller.getStatus())) {
+            String password = seller.getPassword();
+            Set<GrantedAuthority> authorities = new HashSet<>();
+            SimpleGrantedAuthority simpleGrantedAuthority = new SimpleGrantedAuthority("ROLE_SELLER");
+            authorities.add(simpleGrantedAuthority);
+            User user = new User(username, password, authorities);
+            return user;
+        }
+        return null;
+    }
+}
